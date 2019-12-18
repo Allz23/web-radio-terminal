838c5aa8d218b8aa718e30c496c2715bfbd96495
Allz23 <54676417+Allz23@users.noreply.github.com>
2019-12-18 16:41:43 -0400

    Borrada la funcion de validacion de formularios

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,79 +1,115 @@
 $(document).ready(function() {
-  // Document ready, place your code below:
+ // Document ready, place your code below:
 
-  // Variables usadas en el programa.
-  let nombreCheck = $("#nombreCheck");
-  let fechaCheck = $("#fechaCheck");
-  let sesionCheck = $("#sesionCheck");
-  let datePicker = $("#date");
-  let selectNombre = $("#Nombre");
-  let selectSesion = $("#Sesion");
+ // Variables usadas en el programa.
+ let nombreCheck = $("#nombreCheck");
+ let fechaCheck = $("#fechaCheck");
+ let sesionCheck = $("#sesionCheck");
+ let datePicker = $("#date");
+ let selectNombre = $("#Nombre");
+ let selectSesion = $("#Sesion");
 
-  // Inicializaciones
-  nombreCheck.prop("checked", true);
-  datePicker.toggleClass("disabled").prop("disabled", true);
-  selectSesion.toggleClass("disabled").prop("disabled", true);
-  sesionCheck.prop({ disabled: true });
-  fechaCheck.prop({ disabled: true });
+ // Inicializaciones
+ nombreCheck.prop("checked", true);
+ datePicker.toggleClass("disabled").prop("disabled", true);
+ selectSesion.toggleClass("disabled").prop("disabled", true);
+ sesionCheck.prop({ disabled: true });
+ fechaCheck.prop({ disabled: true });
 
-  //Añadimos el comportamineto del checkbox de 'Fecha'
-  $("#fechaCheck").on("click", function() {
-    if (this.checked === true) {
-      datePicker.toggleClass("disabled");
-      datePicker.prop("disabled", false);
-      // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
-      sesionCheck.prop({ disabled: true, checked: false });
-      nombreCheck.prop({ disabled: true, checked: false });
-    } else {
-      datePicker.toggleClass("disabled");
-      datePicker.prop("disabled", true);
-      sesionCheck.prop({ disabled: false, checked: false });
-      nombreCheck.prop({ disabled: false, checked: false });
-    }
-  });
+ //Añadimos el comportamineto del checkbox de 'Fecha'
+ $("#fechaCheck").on("click", function() {
+  if (this.checked === true) {
+   datePicker.toggleClass("disabled");
+   datePicker.prop("disabled", false);
+   // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
+   sesionCheck.prop({
+    disabled: true,
+    checked: false
+   });
+   nombreCheck.prop({
+    disabled: true,
+    checked: false
+   });
+  } else {
+   datePicker.toggleClass("disabled");
+   datePicker.prop("disabled", true);
+   sesionCheck.prop({
+    disabled: false,
+    checked: false
+   });
+   nombreCheck.prop({
+    disabled: false,
+    checked: false
+   });
+  }
+ });
 
-  //Añadimos el comportamineto del checkbox de 'Sesion'
-  $("#sesionCheck").on("click", function() {
-    if (this.checked === true) {
-      selectSesion.toggleClass("disabled");
-      selectSesion.prop("disabled", false);
-      // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
-      fechaCheck.prop({ disabled: true, checked: false });
-      nombreCheck.prop({ disabled: true, checked: false });
-    } else {
-      selectSesion.toggleClass("disabled");
-      selectSesion.prop("disabled", true);
-      fechaCheck.prop({ disabled: false, checked: false });
-      nombreCheck.prop({ disabled: false, checked: false });
-    }
-  });
+ //Añadimos el comportamineto del checkbox de 'Sesion'
+ $("#sesionCheck").on("click", function() {
+  if (this.checked === true) {
+   selectSesion.toggleClass("disabled");
+   selectSesion.prop("disabled", false);
+   // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
+   fechaCheck.prop({
+    disabled: true,
+    checked: false
+   });
+   nombreCheck.prop({
+    disabled: true,
+    checked: false
+   });
+  } else {
+   selectSesion.toggleClass("disabled");
+   selectSesion.prop("disabled", true);
+   fechaCheck.prop({
+    disabled: false,
+    checked: false
+   });
+   nombreCheck.prop({
+    disabled: false,
+    checked: false
+   });
+  }
+ });
 
-  //Añadimos el comportamineto del checkbox de 'Nombre'
-  $("#nombreCheck").on("click", function() {
-    if (this.checked === true) {
-      selectNombre.toggleClass("disabled");
-      selectNombre.prop("disabled", false);
-      // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
-      fechaCheck.prop({ disabled: true, checked: false });
-      sesionCheck.prop({ disabled: true, checked: false });
-    } else {
-      selectNombre.toggleClass("disabled");
-      selectNombre.prop("disabled", true);
-      fechaCheck.prop({ disabled: false, checked: false });
-      sesionCheck.prop({ disabled: false, checked: false });
-    }
-  });
+ //Añadimos el comportamineto del checkbox de 'Nombre'
+ $("#nombreCheck").on("click", function() {
+  if (this.checked === true) {
+   selectNombre.toggleClass("disabled");
+   selectNombre.prop("disabled", false);
+   // Al seleccionar, deshabilitamos los demas checkboxes, y les quitamos la propiedad 'checked'
+   fechaCheck.prop({
+    disabled: true,
+    checked: false
+   });
+   sesionCheck.prop({
+    disabled: true,
+    checked: false
+   });
+  } else {
+   selectNombre.toggleClass("disabled");
+   selectNombre.prop("disabled", true);
+   fechaCheck.prop({
+    disabled: false,
+    checked: false
+   });
+   sesionCheck.prop({
+    disabled: false,
+    checked: false
+   });
+  }
+ });
 
-  // Funcionalidad del selector de fecha
-  $("#date").dateDropper({
-    largeDefault: false,
-    format: "Y-m-d",
-    lang: "es"
-  });
+ // Funcionalidad del selector de fecha
+ $("#date").dateDropper({
+  largeDefault: false,
+  format: "Y-m-d",
+  lang: "es"
+ });
 
-  $("#aExcel").tableExport({
-    formats: ["xlsx"], //Tipo de archivos a exportar ("xlsx","txt", "csv", "xls")
-    bootstrap: true, //Usar lo estilos de css de bootstrap para los botones (true, false)
-    fileName: "Registro-Diario" //Nombre del archivo,
-  });
+ $("#aExcel").tableExport({
+  formats: ["xlsx"], //Tipo de archivos a exportar ("xlsx","txt", "csv", "xls")
+  bootstrap: true, //Usar lo estilos de css de bootstrap para los botones (true, false)
+  fileName: "Registro-Diario" //Nombre del archivo,
+ });
 });
